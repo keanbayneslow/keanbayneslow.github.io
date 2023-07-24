b340be45548296e67d277a178bcacf64cab5ba0a
Kean Bayneslow <kean.bayneslow@gmail.com>
2023-07-24 18:18:15 +1000

    added array for greetings

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+const greetings = [
+    "Hello,",
+    "Welcome,",
+    "Hi there,",
+    "Greetings,",
+    "Hey,",
+    "Nice to meet you,",
+];
+
 function greetUser() {
     const userName = prompt("What is your name?");
     if (userName) {
         const greetingElement = document.getElementById("greeting");
-        greetingElement.textContent = `Welcome, ${userName}!`;
+        const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+        greetingElement.textContent = `${randomGreeting} ${userName}!`;
 
-        const navbar = document.querySelector(".navbar");
-        navbar.appendChild(greetingElement);
+        greetingElement.classList.add("greeting-show");
 
         const button = document.querySelector("button");
         button.style.display = "none";
-
     }
 }
+
+
