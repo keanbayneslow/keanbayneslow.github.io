52a19c90e8397a3734f5bcc2889a45afc6f92417
Kean Bayneslow <kean.bayneslow@gmail.com>
2023-09-22 15:55:44 +1000

    updates skills, projects and resume

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-const colorThemes = document.querySelectorAll('[name="theme"]');
-const rootElement = document.documentElement;
+/*const themePicker = document.querySelector('.theme-picker');
+const darkRadio = document.querySelector('#dark');
+const lightRadio = document.querySelector('#light');
 
+function handleThemeChange() {
+    if (darkRadio.checked) {
+        document.body.classList.remove('light');
+        document.body.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+    } else if (lightRadio.checked) {
+        document.body.classList.remove('dark');
+        document.body.classList.add('light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+
+darkRadio.addEventListener('change', handleThemeChange);
+lightRadio.addEventListener('change', handleThemeChange);
+
+
+const storedTheme = localStorage.getItem('theme');
+if (storedTheme === 'dark') {
+    darkRadio.checked = true;
+    handleThemeChange();
+} else if (storedTheme === 'light') {
+    lightRadio.checked = true;
+    handleThemeChange();
+}
+
+*/
 
 //user greeting
 const greetings = [
